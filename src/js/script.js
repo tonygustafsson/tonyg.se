0824ad26b5fbfe5c8981266da15c34c36eeef3f0
tony.gustafsson <tony.gustafsson@starrepublic.com>
2020-12-21 15:29:05 +0100

    Add transition image after load

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        const headerImg = document.querySelector('.header__image');
+        headerImg.classList.add('header__image--transitionable');
+    }, 0);
+});
