6746082e4a71d50ebc01b02e3c374688a8af91c1
tony.gustafsson <tony.gustafsson@starrepublic.com>
2022-07-19 21:53:21 +0200

    Simplify dark mode

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,25 @@
-const setDarkMode = () => {
+const toggleDarkMode = () => {
     const nightIcon = document.querySelector('.js-theme-toggler-night');
     const dayIcon = document.querySelector('.js-theme-toggler-day');
 
-    nightIcon.classList.add('active');
-    dayIcon.classList.remove('active');
-
-    document.body.classList.add('dark-mode');
-};
-
-const removeDarkMode = () => {
-    const nightIcon = document.querySelector('.js-theme-toggler-night');
-    const dayIcon = document.querySelector('.js-theme-toggler-day');
-
-    nightIcon.classList.remove('active');
-    dayIcon.classList.add('active');
-
-    document.body.classList.remove('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        // Enable light mode
+        nightIcon.classList.remove('active');
+        dayIcon.classList.add('active');
+        document.body.classList.remove('dark-mode');
+    } else {
+        // Enable dark mode
+        nightIcon.classList.add('active');
+        dayIcon.classList.remove('active');
+        document.body.classList.add('dark-mode');
+    }
 };
 
 const setThemeBasedOnPreferedScheme = () => {
     const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
 
     if (prefersDarkScheme.matches) {
-        setDarkMode();
-    } else {
-        removeDarkMode();
+        toggleDarkMode();
     }
 };
 
@@ -35,19 +30,11 @@ const addImageTransitions = () => {
     }, 0);
 };
 
-const toggleTheme = () => {
-    if (document.body.classList.contains('dark-mode')) {
-        removeDarkMode();
-    } else {
-        setDarkMode();
-    }
-};
-
 const addThemeToggleTriggers = () => {
     const themeTogglers = Array.from(document.querySelectorAll('.js-theme-toggler'));
 
     themeTogglers.forEach((themeToggler) => {
-        themeToggler.addEventListener('click', toggleTheme);
+        themeToggler.addEventListener('click', toggleDarkMode);
     });
 };
 
